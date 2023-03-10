7bfcabc03ff835d80c1d9910136467e15c83f050
rameshgirka-afl <rameshgirka@gmail.com>
2023-03-10 09:33:00 +0530

    initia commit to TypeScript asus windows

---
+++ basics-src/index.ts
@@ -0,0 +1,25 @@
+// any 
+
+const var_name:String = 'string_only'
+let any_var;
+
+function safeMethod(input_param: any ){
+
+}
+
+// Arrays 
+
+
+
+// Tuples 
+
+
+
+// Enums 
+
+
+
+// functions 
+
+
+//Objects 
